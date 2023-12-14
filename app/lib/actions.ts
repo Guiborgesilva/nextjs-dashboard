133ada6866d7a61e9f9a1b36670ef8cb444123a2
Guiborgesilva <guiborgesilva@outlook.com>
2023-12-14 15:58:36 -0300

    Finalização do app, criar tela de login

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -66,13 +66,21 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
  
 // ...
  
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  if(!validatedFields.success){
+    return{
+      error: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed  to Update Invoice'
+    }
+  }
  
+  const { customerId, amount, status } = validatedFields.data
   const amountInCents = amount * 100;
  
   try{
@@ -92,7 +100,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string){
-  throw new Error('Failed to Delete Invoice');
   try{
     await sql `DELETE FROM invoices WHERE id = ${id}`
     revalidatePath('/dashboard/invoices')
